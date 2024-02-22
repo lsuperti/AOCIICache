4d1c2bc5dae85993a247282fa293d71b899ecddc
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2024-02-22 20:58:10 -0300

    Add error checking to number parameter parsing
    When number parameter strings were parsed to get integers the old atoi
    function was used. It was replaced by the strtol function, with error
    handling.
    
    The atoi function has several problems and should be generally avoided
    in all cases. The problems include the complete lack of error and
    security checking, making the program prone to several exploits and
    generally unreliable.
    
    For example, if another program, without elevated privileges, for some
    reason, could run this program with elevated privileges, privilege
    escalation could be trivially achieved by passing a well-crafted string
    as a number parameter that would be passed unsanitized to the atoi
    function. Other than that, if remotely running this program with
    arbitrary arguments is possible, remote code execution would also be
    trivially achievable the same way.
    
    The atoi function also has no error checking or signaling, so if the
    number string is not valid its return value is undefined, so it could be
    a usable value that has nothing to do with the intended value in the
    input string. The strtol function now used has error checking and its
    errors are handled by exiting the program with an error message.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include <inttypes.h>
 #include <math.h>
 #include <string.h>
+#include <errno.h>
+#include <assert.h>
 
 #include "FileHandler.h"
 #include "Simulator.h"
@@ -14,23 +16,23 @@ enum outFlag_t {
 };
 
 void printOutput( int flagOut, result_t result );
+long handleNumberInput( char * input, int index );
 
 int main( int argc, char *argv[] ) {
 	char * quote = strchr( argv[ 0 ], ' ' ) == NULL ? "" : "\"";
 	
 	if ( argc != 7 ) {
 		printf( "Número de argumentos incorreto. Utilize:\n" );
-		printf( "%s%s%s <nsets> <bsize> <assoc> <substituição> <flag_saída> arquivo_de_entrada\n", quote, argv[ 0 ], quote );
+		printf( "%s%s%s <nsets> <bsize> <assoc> <substituição> <flag_saída> <arquivo_de_entrada>\n", quote, argv[ 0 ], quote );
 		exit( EXIT_FAILURE );
 	}
 
-	int          nsets = atoi( argv[ 1 ] );
-	int          bsize = atoi( argv[ 2 ] );
-	int          assoc = atoi( argv[ 3 ] );
-	char *       subst = argv[ 4 ];
-	int          flagOut = atoi( argv[ 5 ] );
-	char *       arquivoEntrada = argv[ 6 ];
-	result_t	 result;
+	uint32_t  nsets = ( uint32_t )handleNumberInput( argv[ 1 ], 1 );
+	int       bsize = ( uint32_t )handleNumberInput( argv[ 2 ], 2 );
+	int       assoc = ( uint32_t )handleNumberInput( argv[ 3 ], 3 );
+	char *    subst = argv[ 4 ];
+	int       flagOut = ( uint32_t )handleNumberInput( argv[ 5 ], 5 );
+	char *    arquivoEntrada = argv[ 6 ];
 
 	printf( "nsets = %d\n", nsets );
 	printf( "bsize = %d\n", bsize );
@@ -44,6 +46,8 @@ int main( int argc, char *argv[] ) {
     
     handleFile( arquivoEntrada, &addresses, &size );
 
+	result_t  result;
+	
 	if ( assoc == 1 ) {
 		result = directMapping( addresses, size, bsize, nsets );
 	}
@@ -88,4 +92,33 @@ void printOutput( int flagOut, result_t result ) {
 	} else {
 		printf( "%d, %f, %f, %f, %f, %f\n", result.accesses, hitRate, missRate, compulsoryMissRate, capacityMissRate, conflictMissRate );
 	}
+}
+
+/* This function handles number parameter parsing and error checking.
+ * 
+ * It will use the strtol function to parse the input string.
+ * 
+ * The strtol function is considered safe to parse user-provided strings, as it will do error and security checks. (See the commit that added this function for more information and examples of potential exploits.) (Change "the commit that added this function" to the actual commit number if you see this after it has been commited.)
+ * 
+ * The strtol function will automatically handle hexadecimal and octal numbers if the input string starts with "0x" or "0" respectively (and binary (0b) if the program is compiled with C2x).
+ */
+long handleNumberInput( char * input, int index ) {
+	long number;
+	char * endptr;
+	const char * args[] = { NULL, "<nsets>", "<bsize>", "<assoc>", "<substituição>", "<flag_saída>", "<arquivo_de_entrada>" };
+
+	static_assert( sizeof( long ) >= sizeof( uint32_t ), "A long isn't a least 32 bits that allow any acceptable parameter values parameters to be taken. Consider using long long instead." );
+	static_assert( ERANGE != 0, "ERANGE (range-related errors) is 0, that means it will always report an error when handling the number input, since it checks if errno is ERANGE and errno is set to 0 before the string handling that could cause the error to ensure a previous error is not caught inadvertently. A solution is to set the errno to some other value, but that may cause problems elsewhere." );
+
+	errno = 0;
+
+	// Use base 0 to automatically allow hexadecimal and octal numbers (and binary if compiling with C2x).
+	number = strtol( input, &endptr, 0 );
+	
+	if ( *endptr != '\0' || endptr == input || errno == ERANGE ) {
+		printf( "Erro: argumento \"%s\" no parâmetro %s não é um número válido ou aceitável.\n", input, args[ index ] );
+		exit( EXIT_FAILURE );
+	}
+	
+	return number;
 }
