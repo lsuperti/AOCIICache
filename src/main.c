dd20783fd2a0c797204f701d5f9829edf150640f
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2024-03-01 17:40:44 -0300

    Change input number parsing to use strtoul
    The input number parsing was using strtol to parse the input number,
    instead of strtoul. The strtoul function works with unsigned longs
    instead of longs, which is more appropriate since the input numbers are
    always positive.
    
    The handleNumberInput function was also renamed to parseNumberInput to
    improve clarity. Comments in it were also change to reflect that it
    parses the number input.

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ enum outFlag_t {
 };
 
 void printOutput( int flagOut, result_t result );
-long handleNumberInput( char * input, int index );
+long parseNumberInput( char * input, int index );
 int parseReplacementPolicy( char * subst );
 
 int main( int argc, char *argv[] ) {
@@ -30,11 +30,11 @@ int main( int argc, char *argv[] ) {
         exit( EXIT_FAILURE );
     }
 
-    uint32_t  nsets = ( uint32_t )handleNumberInput( argv[ 1 ], 1 );
-    uint32_t  bsize = ( uint32_t )handleNumberInput( argv[ 2 ], 2 );
-    uint32_t  assoc = ( uint32_t )handleNumberInput( argv[ 3 ], 3 );
+    uint32_t  nsets = ( uint32_t )parseNumberInput( argv[ 1 ], 1 );
+    uint32_t  bsize = ( uint32_t )parseNumberInput( argv[ 2 ], 2 );
+    uint32_t  assoc = ( uint32_t )parseNumberInput( argv[ 3 ], 3 );
     char *    subst = argv[ 4 ];
-    int       flagOut = ( int )handleNumberInput( argv[ 5 ], 5 );
+    int       flagOut = ( int )parseNumberInput( argv[ 5 ], 5 );
     char *    arquivoEntrada = argv[ 6 ];
 
     printf( "nsets = %d\n", nsets );
@@ -101,26 +101,26 @@ void printOutput( int flagOut, result_t result ) {
     }
 }
 
-/* This function handles number parameter parsing and error checking.
+/* This function parses number parameters with and error checking.
  * 
- * It will use the strtol function to parse the input string.
+ * It will use the strtoul function to parse the input string.
  * 
- * The strtol function is considered safe to parse user-provided strings, as it will do error and security checks.
+ * The strtoul function is considered safe to parse user-provided strings, as it will do error and security checks.
  * 
- * The strtol function will automatically handle hexadecimal and octal numbers if the input string starts with "0x" or "0" respectively (and binary ("0b") if the program is compiled with C2x).
+ * The strtoul function will automatically handle hexadecimal and octal numbers if the input string starts with "0x" or "0" respectively (and binary ("0b") if the program is compiled with C2x).
  */
-long handleNumberInput( char * input, int index ) {
-    long number;
-    char * endptr;
-    const char * args[] = { NULL, "<nsets>", "<bsize>", "<assoc>", "<substituição>", "<flag_saída>", "<arquivo_de_entrada>" };
+long parseNumberInput( char * input, int index ) {
+    unsigned long number;
+    char *        endptr;
+    const char *  args[] = { NULL, "<nsets>", "<bsize>", "<assoc>", "<substituição>", "<flag_saída>", "<arquivo_de_entrada>" };
 
-    static_assert( sizeof( long ) >= sizeof( uint32_t ), "A long isn't a least 32 bits that allow any acceptable parameter values parameters to be taken. Consider using long long instead." );
-    static_assert( ERANGE != 0, "ERANGE (range-related errors) is 0, that means it will always report an error when handling the number input, since it checks if errno is ERANGE and errno is set to 0 before the string handling that could cause the error to ensure a previous error is not caught inadvertently. A solution is to set the errno to some other value, but that may cause problems elsewhere." );
+    static_assert( sizeof( unsigned long ) >= sizeof( uint32_t ), "A unsigned long isn't a least 32 bits that allow any acceptable parameter values parameters to be taken. Consider using unsigned long long instead." );
+    static_assert( ERANGE != 0, "ERANGE (range-related errors) is 0, that means it will always report an error when parsing the number input, since it checks if errno is ERANGE and errno is set to 0 before the string handling that could cause the error to ensure a previous error is not caught inadvertently. A solution is to set the errno to some other value, but that may cause problems elsewhere." );
 
     errno = 0;
 
     // Use base 0 to automatically allow hexadecimal and octal numbers (and binary if compiling with C2x).
-    number = strtol( input, &endptr, 0 );
+    number = strtoul( input, &endptr, 0 );
     
     if ( *endptr != '\0' || endptr == input || errno == ERANGE ) {
         printf( "Erro: argumento \"%s\" no parâmetro %s não é um número válido ou aceitável.\n", input, args[ index ] );
